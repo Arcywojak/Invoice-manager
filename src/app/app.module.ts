99ad16b57286662f6b1f4b8de82c18d73d330570
maciej778 <maciej778TEST@gmail.com>
2021-08-02 22:41:53 +0200

    create main components (navbar and invoice manager) and add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+import { InvoiceManagerComponent } from './features/invoice-manager/invoice-manager.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InvoiceManagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
